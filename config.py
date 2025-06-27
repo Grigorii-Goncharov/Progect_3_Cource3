7533ef514e2fd8f9ed4c3a70418d5860a6c01c58
grigorii goncharov <grigoriy85@gmail.com>
2025-06-27 14:29:39 +0300

    1. функция load_company для получения id компании через api

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36"
+
+# config.py
+DB_CONFIG = {
+    'dbname': 'hh_vacancies',
+    'user': 'postgres',
+    'password': 12345,
+    'host': 'localhost',
+    'port': 5432
+}
